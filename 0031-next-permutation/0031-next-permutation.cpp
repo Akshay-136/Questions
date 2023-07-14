2b18ac1bd924fa0a671d033bc5bf412bf49f0628
Akshay Chandel <115414944+Akshay-136@users.noreply.github.com>
2023-07-14 23:05:08 +0530

    Time: 3 ms (82.44%), Space: 11.9 MB (92.69%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n= nums.size();
+        vector<int> ans;
+        next_permutation(nums.begin(),nums.begin()+n);
+        
+    
+    }
+    
+};
